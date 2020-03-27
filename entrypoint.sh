9ad5d3f045887cef3f3ad1da1c2fc3517a79f97b
Mardari Igor <igor.mardari96@gmail.com>
2020-03-27 03:00:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ for e in $(env); do
   k=$(echo $e | cut -d= -f1)
   v=$(echo $e | sed 's/^.*=//')
   if [ "x$k" = "x${k#GITHUB_}" ]; then
-    echo "export $k='$v'" >> $GITHUB_WORKSPACE/.env
+    echo "$k=$v" >> $GITHUB_WORKSPACE/.env
   fi
 done
